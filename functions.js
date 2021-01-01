a387d091b0ffefcda06a4d70cea4a5ce9c4b0926
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2020-12-31 21:14:10 -0600

    created guesses function

--- functions.js
+++ functions.js
@@ -46,3 +46,25 @@ window.onload = function () {
             catagoryName.innerHTML = "The Chosen Category Is Cities";
         }
     }
+
+    // Create geusses ul
+    result = function () {
+        wordHolder = document.getElementById('hold');
+        correct = document.createElement('ul');
+
+        for (var i = 0; i < word.length; i++) {
+            correct.setAttribute('id', 'my-word');
+            guess = document.createElement('li');
+            guess.setAttribute('class', 'guess');
+            if (word[i] === "-") {
+                guess.innerHTML = "-";
+                space = 1;
+            } else {
+                guess.innerHTML = "_";
+            }
+
+            geusses.push(guess);
+            wordHolder.appendChild(correct);
+            correct.appendChild(guess);
+        }
+    }
