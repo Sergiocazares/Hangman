8382241edbe012a09bbae2cac1cca9cade986e8c
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-01-06 22:28:35 -0600

    created reset function

--- functions.js
+++ functions.js
@@ -218,3 +218,13 @@ hint.onclick = function() {
     var hintIndex = chosenCategory.indexOf(word);
     showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
 };
+
+// Reset
+
+document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+}
